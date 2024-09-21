97a4ac5d9679f2b0a8bdc8aecd488616f0b7137d
Santosh Suvarna <153625569+santytattoo@users.noreply.github.com>
2024-09-21 09:40:25 +0400

    Umbraco updates

---
+++ PWCProject/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace PWCProject.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static HomePage? GetHomePage(this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<HomePage>();
+        }
+        public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent)
+        {
+            var homePage = GetHomePage(publishedContent);
+            if (homePage == null)
+            {
+                return null;
+            }
+            return homePage.FirstChild<SiteSettings>();
+        }
+    }
+}
